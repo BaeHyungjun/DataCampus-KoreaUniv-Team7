d9f4e883fbf182909d79b8a2f60773ae8e080875
Eunkyung Koh <ekherb1244@gmail.com>
2020-08-28 10:40:46 +0900

    Update flask frontend code

--- flask/youtube_summary/app.py
+++ flask/youtube_summary/app.py
@@ -3,15 +3,16 @@ import sqlite3
 
 app = Flask(__name__)
 
+conn = sqlite3.connect('youtubenews.db')
+conn.row_factory = sqlite3.Row
+cur = conn.cursor()
+cur.execute("SELECT * FROM youtubenews")
+rows = cur.fetchall()
+rows = rows[:50]  # 최근50개만 추출
+
 
 @app.route('/')
 def home():
-    conn = sqlite3.connect('youtubenews.db')
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM youtubenews")
-    rows = cur.fetchall()
-    rows = rows[:50] #최근50개만 추출
     return render_template("index.html", rows=rows)
 
 
@@ -24,6 +25,45 @@ def report():
 def about():
     return render_template('index3.html')
 
+
+@app.route('/ytn', methods=['GET', 'POST'])
+def ytn():
+    return render_template("index_ytn.html", rows=rows)
+
+
+@app.route('/kbs', methods=['GET', 'POST'])
+def kbs():
+    return render_template("index_kbs.html", rows=rows)
+
+
+@app.route('/mbc', methods=['GET', 'POST'])
+def mbc():
+    return render_template("index_mbc.html", rows=rows)
+
+
+@app.route('/sbs', methods=['GET', 'POST'])
+def sbs():
+    return render_template("index_sbs.html", rows=rows)
+
+
+@app.route('/yhnews', methods=['GET', 'POST'])
+def yhnews():
+    return render_template("index_yhnews.html", rows=rows)
+
+
+@app.route('/jtbc', methods=['GET', 'POST'])
+def jtbc():
+    return render_template("index_jtbc.html", rows=rows)
+
+
+@app.route('/channel_a', methods=['GET', 'POST'])
+def channel_a():
+    return render_template("index_channel_a.html", rows=rows)
+
+
+@app.route('/mbn', methods=['GET', 'POST'])
+def mbn():
+    return render_template("index_mbn.html", rows=rows)
 
 if __name__ == '__main__':
     app.run()
